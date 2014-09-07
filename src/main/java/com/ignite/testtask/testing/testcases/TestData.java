64993d916740e1be965b94a21156e9b3ae36dc4f
Dean <dean.felmly@gmail.com>
2014-09-07 23:55:26 +0200

    Added Missing File from previous commit.

---
+++ src/main/java/com/ignite/testtask/testing/testcases/TestData.java
@@ -0,0 +1,11 @@
+package com.ignite.testtask.testing.testcases;
+
+public class TestData {
+	
+	public static final String TARGET_URL = "http://ignitetestingtask.felmlyd.cloudbees.net/";
+	
+	public static final String HEADER = "/html/head";
+	public static final String NEW_TEAM_XPATH = "/html/body/p/a[1]";
+	public static final String TEAM_LIST_XPATH = "/html/body/p/a[2]";
+	
+}
